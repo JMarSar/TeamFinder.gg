f6bb86309819cfbc55f7fd646d6f0d8fe7ecb382
Panigc93 <panigc93@gmail.com>
2021-12-05 18:49:07 +0100

    creado login manager y retocadas pantallas de admin

---
+++ src/app/pages/crear-admin/crear-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-admin',
+  templateUrl: './crear-admin.component.html',
+  styleUrls: ['./crear-admin.component.css']
+})
+export class CrearAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
