fc8d5bdc16f880d468fa93452bf19a8fd7ccffec
Protoboronco <izan.gonz@gmail.com>
2021-12-09 10:59:22 +0100

    subida de registo con correcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { ChatComponent } from './pages/chat/chat.component';
 import { RankingComponent } from './pages/ranking/ranking.component';
 import { ListaChatsComponent } from './pages/lista-chats/lista-chats.component';
 import { CreateTournamentComponent } from './pages/create-tournament/create-tournament.component';
+import { RegistroFavComponent } from './pages/registro-fav/registro-fav.component';
 
 @NgModule({
   declarations: [
@@ -78,6 +79,7 @@ import { CreateTournamentComponent } from './pages/create-tournament/create-tour
     BuscarScrimComponent,
     ListaChatsComponent,
     CreateTournamentComponent,
+    RegistroFavComponent
   ],
   imports: [
     BrowserModule,
