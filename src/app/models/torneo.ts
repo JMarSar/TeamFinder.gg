53015b2d9219ec4404ebf045cf91e30e1c285c54
RaulBakiMartin <raulbakiprogramacion@gmail.com>
2021-12-17 10:43:53 +0100

    cambio orden torneos

--- src/app/models/torneo.ts
+++ src/app/models/torneo.ts
@@ -1,9 +1,16 @@
 import { Partida } from './partida';
 export class Torneo {
         public partida:Partida;
+        public partidas:Partida[];
 
         constructor(public nombre:string, public tier:string, public fecha:string, public patrocinado:boolean){
-                this.partida = new Partida("eArrows", "Team Comilona", "3", "2")
+                this.partida = new Partida("eArrows", "--NOMADS--", "3", "2")
+                // this.partidas = [
+                //         new Partida ("nombre de prueba","prueba2","0","9"),
+                //         new Partida ("nombre de prueba","prueba2","0","9"),
+                //         new Partida ("nombre de prueba","prueba2","0","9"),
+                //         new Partida ("nombre de prueba","prueba2","0","9"),
+                // ]
         }
         
 }
